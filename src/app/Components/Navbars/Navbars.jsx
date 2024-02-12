ecfe158e34464196befd31bcbfcb20b4d1f082ea
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 11:00:45 +0200

    new code is added now

--- src/app/Components/Navbars/Navbars.jsx
+++ src/app/Components/Navbars/Navbars.jsx
@@ -6,7 +6,20 @@ export default function Navbars() {
     <div className="n-wrapper">
         <div className='container'>
              <div className="n-container">
-                Navbars
+              {/* Left section */}
+              <div className="n-logo font-bold text-white">
+                <span>Maket design</span>
+              </div>
+              {/* Right section */}
+              <div className="n-right flex items-center gap-3 justify-center text-sm">
+                 <div className="n-menu flex items-center gap-3 justify-center">
+                    <span>What we do</span>
+                    <span>How we works</span>
+                    <span>Our Markting</span>
+                    <span>Testimonial</span>
+                 </div>
+                 <div className="fund-button text-sm">button</div>
+              </div>
              </div>
         </div>
     </div>
