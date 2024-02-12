3593358d39a3c73858238f37ce29d1ccf9600729
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 11:10:29 +0200

    new code is added now

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-        
+    <div className="h-wrapper">
+        <div className="container">
+            <div className="h-container">
+                Hero section
+            </div>
+        </div>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import './globals.css'
 import Navbars from "./Components/Navbars/Navbars";
+import Hero from "./Components/Hero/Hero";
 
 export default function Home() {
   return (
     <main className="app">
       <Navbars />
+      <Hero />
     </main>
   );
 }
