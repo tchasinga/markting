c52015eaaaf14b1ab90f0ced4a1bdda5fa42546b
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 12:46:21 +0200

    new code is added now

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Hero.css'
 import EmailBox from '../EmailBox/EmailBox'
+import heroData from "../../Utils/data.js"
+import Image from "next/image";
 
 export default function Hero() {
   return (
@@ -9,7 +11,31 @@ export default function Hero() {
             <div className="h-container">
                {/* Left section */}
               
-               <div className="h-left">Left Section</div>
+               <div className="h-left">
+                 <div className="image-row">
+                  {
+                    heroData.slice(0, 3).map((person , i) =>(
+                      <div className="person-pill" key={i}>
+                        <div className="person-pill-bg">
+                           <Image src={person.src} alt={person.src}/>
+                        </div>
+                      </div>
+                    ))
+                  }
+                 </div>
+
+                 <div className="image-row">
+                  {
+                    heroData.slice(3, 6).map((person , i) =>(
+                      <div className="person-pill" key={i}>
+                        <div className="person-pill-bg">
+                           <Image src={person.src} alt={person.src}/>
+                        </div>
+                      </div>
+                    ))
+                  }
+                 </div>
+               </div>
 
                {/* Right section */}
                <div className="h-right">
