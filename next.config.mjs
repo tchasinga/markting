19bbcc20964b77f162c70bcc5beab57bcce61a78
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 14:29:26 +0200

    new code is now

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Add file-loader rule for MP4 files
+        config.module.rules.push({
+          test: /\.(mp4|webm)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              publicPath: '/_next/static/videos/', // adjust the publicPath as needed
+              outputPath: 'static/videos/', // adjust the outputPath as needed
+              esModule: false,
+            },
+          },
+        });
+    
+        // Fix for serverless environments, like Vercel
+        if (!isServer) {
+          config.resolve.fallback.fs = false;
+        }
+    
+        return config;
+      },
+};
+
 
 export default nextConfig;
--- src/app/Components/BrandingVideo/BrandingVideo.jsx
+++ src/app/Components/BrandingVideo/BrandingVideo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BrandingVideo() {
   return (
     <div>
-        
+        balole
     </div>
   )
 }
