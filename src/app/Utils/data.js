73ad4e73aac34b3fc25c2d9b7c0b75c97bb4b04a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 12:27:25 +0200

    new changes is added now

---
+++ src/app/Utils/data.js
@@ -0,0 +1,46 @@
+import personOne from "../assets/img/personOne.jpg";
+import personTwo from "../assets/img/personTwo.jpg";
+import personThree from "../assets/img/personThree.jpg";
+import personFour from "../assets/img/personFour.jpg";
+import personFive from "../assets/img/personFive.jpg";
+import personSix from "../assets/img/personSix.jpg";
+
+const heroData = [
+  {
+    src: personOne,
+    bg: "#6bb8c2",
+    delay: 0.1,
+  },
+
+  {
+    src: personTwo,
+    bg: "#dddbdc",
+    delay: 0.3,
+  },
+
+  {
+    src: personThree,
+    bg: "#fec045",
+    delay: 0.2,
+  },
+
+  {
+    src: personFour,
+    bg: "#f8f6f7",
+    delay: 0.4,
+  },
+
+  {
+    src: personFive,
+    bg: "#bcdfe3",
+    delay: 0.5,
+  },
+
+  {
+    src: personSix,
+    bg: "#ecadc8",
+    delay: 0.6,
+  },
+];
+
+export default heroData;
