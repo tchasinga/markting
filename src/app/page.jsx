e02c61dd7fe8471202b4f00e4d58b630ae047956
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 14:22:41 +0200

    new code is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import './globals.css'
 import Navbars from "./Components/Navbars/Navbars";
 import Hero from "./Components/Hero/Hero";
+import BrandingVideo from "./Components/BrandingVideo/BrandingVideo";
 
 export default function Home() {
   return (
     <main className="app">
       <Navbars />
       <Hero />
+      <BrandingVideo/>
     </main>
   );
 }
