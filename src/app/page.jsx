956cf4bf844c55116ec6db2de48a809cc6748b6a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 09:22:46 +0200

    new code is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
+      <h1 className="text-4xl font-bold text-center">Welcome to Next.js</h1>
     </main>
   );
 }
