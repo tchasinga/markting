86a454b800e599ccccf3ae6b91272b82b095ac06
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-12 11:19:55 +0200

    New code is added now

--- src/app/Components/Hero/Hero.jsx
+++ src/app/Components/Hero/Hero.jsx
@@ -6,7 +6,19 @@ export default function Hero() {
     <div className="h-wrapper">
         <div className="container">
             <div className="h-container">
-                Hero section
+               {/* Left section */}
+               <div className="h-left">Left Section</div>
+
+               {/* Right section */}
+               <div className="h-title">
+                <span>Managing How</span>
+                <span>You grow your</span>
+                <span>Digital Markte</span>
+               </div>
+
+               <div className="h-des">
+               Marketing refers to the activities a company undertakes to promote the buying or selling of a product or service, including advertising, selling, and delivering products to consumers or other businesses
+               </div>
             </div>
         </div>
     </div>
