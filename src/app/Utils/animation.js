4ba756ccaaf9c88b2030ffc9a86775204d70bb64
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-13 13:01:41 +0200

    new code is added

--- src/app/Utils/animation.js
+++ src/app/Utils/animation.js
@@ -1,30 +1,46 @@
-export const containerVariants = (delay = 0) =>({
-    "offscreen":{
-        opacity:0,
-        y: 30
+export const containerVariants = (delay = 0) => ({
+  offscreen: {
+    opacity: 0,
+    y: 30,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      duration: 2,
+      delay,
     },
-    "onscreen":{
-        opacity: 1,
-        y:0,
-        transition:{
-            type: "spring",
-            duration: 2,
-            delay
-        }
-    }
-})
+  },
+});
 
 export const imageVariantsTitle = {
-    "offscreen":{
-        opacity:0,
-       y: 30
+  offscreen: {
+    opacity: 0,
+    y: 30,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      duration: 3,
     },
-    "onscreen":{
-        opacity: 1,
-        y:0,
-        transition:{
-            type: "spring",
-            duration: 2,
-        }
-    }
-}
+  },
+};
+
+export const imageVariants = {
+  offscreen: {
+    opacity: 0,
+    y: 20,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      duration: 3,
+      delay: .2,
+    },
+  },
+};
