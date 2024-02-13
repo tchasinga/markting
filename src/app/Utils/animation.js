dcc2e2fc31a710894bf4f7fa166641d2eec87469
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-13 12:59:56 +0200

    new code is added now

--- src/app/Utils/animation.js
+++ src/app/Utils/animation.js
@@ -13,3 +13,18 @@ export const containerVariants = (delay = 0) =>({
         }
     }
 })
+
+export const imageVariantsTitle = {
+    "offscreen":{
+        opacity:0,
+       y: 30
+    },
+    "onscreen":{
+        opacity: 1,
+        y:0,
+        transition:{
+            type: "spring",
+            duration: 2,
+        }
+    }
+}
