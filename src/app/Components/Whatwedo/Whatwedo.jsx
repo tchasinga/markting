61c7e4429a5454aa123496f5c642750500ce2aae
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-13 11:10:20 +0200

    new code is added now

--- src/app/Components/Whatwedo/Whatwedo.jsx
+++ src/app/Components/Whatwedo/Whatwedo.jsx
@@ -6,7 +6,7 @@ export default function Whatwedo() {
     <div className="wwd-wrapper">
         <div className="container">
             <div className="wwd-container">
-                <div className="wwd-content">
+                <div className="wwd-head">
                     <span className="tag">What we do</span>
                     <span className="title">We are a team of professionals who are passionate about what we do. We offer a wide range of services to meet your needs. We are here to help you grow your business and make your dreams a reality. Our services include:</span>
                     <span className="des">Immediate and 100% Guaranteed</span> 
